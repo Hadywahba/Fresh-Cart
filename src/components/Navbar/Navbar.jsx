167d6ae57b2d61c91493a4cb13cf720129f7d0ab
Hadywahba <hadywahba19@gmail.com>
2025-03-05 17:22:21 +0200

    Final8

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,10 +35,10 @@ export default function Navbar() {
   function gotoWishlist(){
     navigate('/Wishlist')
   }
-  // function toggleButton(){
-  //   setopenButton(!openButton)
+  function toggleButton(){
+    setopenButton(!openButton)
    
-  // }
+  }
   return (
 
     <>
@@ -52,10 +52,10 @@ export default function Navbar() {
    <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img onClick={goto} className=' w-[80px]  md:w-[150px]' src={logo} alt="" />
     </a>
-    <div className= {`${openButton ? 'block' : 'none'}  hidden w-full absolute md:block md:relative md:top-0 top-[56px]  left-0  md:w-auto`} id="navbar-default">
+    <div className= {` ${openButton ? 'visible' : 'hidden'} md:block  w-full absolute  md:relative md:top-0 top-[56px]  left-0  md:w-auto`} >
    {token ?   
    <>
-   <ul className={   ` font-xl  bg-[rgb(242,242,248)] md:flex flex-col text-center  items-center justify-center p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row  md:space-x-4 rtl:space-x-reverse md:mt-0 md:border-0 `}>
+   <ul className={   ` font-xl  bg-[rgb(242,242,248)] md:flex flex-col text-center  items-center justify-center p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row  md:space-x-4 rtl:space-x-reverse md:mt-0 md:border-0  `}>
         <li className='pb-2'>
           <NavLink to={''} className="   text-gray-900   " aria-current="page">Home</NavLink>
         </li>
@@ -116,7 +116,7 @@ export default function Navbar() {
 
 
 
-{token ? <button onClick={()=>setopenButton(true)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+{token ? <button onClick={toggleButton}  type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
   <span className="sr-only">Open main menu</span>
   <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
